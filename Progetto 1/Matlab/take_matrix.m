0c88eafcc587a606de3578a782368a7db59b325b
Kevin Manella <kevinmanella1998@gmail.com>
2021-05-31 17:00:38 +0200

    Modifiche varie codice Matlab

--- Progetto 1/Matlab/take_matrix.m
+++
@@ -1,7 +0,0 @@
-disp("Matrice =  TSC_OPF_1047");
-[Dimensione,Tempo,Errore,Memoria] = script_prog1("TSC_OPF_1047")
-
-disp("Matrice =  ns3Da");
-[Dimensione,Tempo,Errore,Memoria] = script_prog1("ns3Da")
-
-
