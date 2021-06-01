05ca1eb386005e411db8b2a39ca48d77447c5998
Kevin Manella <kevinmanella1998@gmail.com>
2021-06-02 01:30:39 +0200

    COMMIT FUNZIONANTE
    Matlab funzionante su Windows
    Python funzionante su Windows
    Da fare Matlab e Python su Linux

--- Progetto 1/Python/script_python.py
+++ Progetto 1/Python/script_python.py
@@ -1,17 +1,22 @@
+import os
+import time
+import psutil
 import numpy
+import scipy
 import scipy.io
 import scipy.sparse
 import scipy.sparse.linalg
-import time
-import os
-import psutil
+import cvxopt
+import cvxopt.umfpack
+import cvxopt.cholmod
+import cvxpy
 
-# Funzione che riceve in input la matrice del sistema lineare e restituisce:
+# Funzione per matrici generiche che riceve in input la matrice del sistema lineare e restituisce:
 # - Dimensione della matrice
 # - Tempo impiegato per risolvere il sistema lineare (in secondi)
 # - Errore relativo della soluzione
 # - Memoria usata per risolvere il sistema lineare (in MB)
-def script_python(matrice):
+def matrix_solver(matrice):
     
     # Caricamento matrice A
     A = scipy.io.mmread(matrice).tocsc()
@@ -31,14 +36,7 @@ def script_python(matrice):
 
     # Risoluzione sistema lineare e tempo impiegato per risolverlo (in secondi)
     tempo_iniziale = time.time()
-    
-    # Per matrici generiche
     x = scipy.sparse.linalg.spsolve(A, b)
-    
-    # Per matrici simmetriche e definite positive (DA SISTEMARE, NON FUNZIONA)
-    #R = scipy.linalg.cholesky(A)
-    #y = scipy.sparse.linalg.spsolve_triangular(R.T, b)
-    #x = scipy.sparse.linalg.spsolve_triangular(R, y)
     tempo_impiegato = time.time() - tempo_iniziale
     
     # Memoria usata dopo aver risolto il sistema lineare (in MB)
@@ -57,29 +55,60 @@ def script_python(matrice):
     print("Errore relativo:", errore_relativo)
     print("Memoria usata:", memoria_usata)
 
-# Richiamo della funzione per ogni matrice
+# Funzione per matrici simmetriche e definite positive che riceve in input la matrice del sistema lineare e restituisce:
+# - Dimensione della matrice
+# - Tempo impiegato per risolvere il sistema lineare (in secondi)
+# - Errore relativo della soluzione
+# - Memoria usata per risolvere il sistema lineare (in MB)
+def matrix_solver_cholesky(matrice):
+    
+    # Caricamento matrice A
+    A = cvxpy.interface.matrix_utilities.sparse2cvxopt(scipy.io.loadmat(matrice)['Problem']['A'][0][0])
+
+    # Memoria usata dopo aver letto la matrice (in MB)
+    user = psutil.Process(os.getpid())
+    memoria_dopo_lettura_matrice = user.memory_info().rss/1e+6
+
+    # Dimensione della matrice
+    dimensione = str(A.size[0])
+    
+    # Soluzione esatta, avente tutte le componenti uguali a 1
+    xe = cvxopt.matrix(numpy.ones([A.size[0], 1]))
+    
+    # Vettore dei termini noti
+    b = cvxopt.sparse(A*xe)
+
+    # Risoluzione sistema lineare e tempo impiegato per risolverlo (in secondi)
+    tempo_iniziale = time.time()
+    x = cvxopt.cholmod.splinsolve(A,b)
+    tempo_impiegato = time.time() - tempo_iniziale
+    
+    # Memoria usata dopo aver risolto il sistema lineare (in MB)
+    user = psutil.Process(os.getpid())
+    memoria_dopo_risoluzione_sistema = user.memory_info().rss/1e+6
+
+    # Errore relativo della soluzione
+    errore_relativo = numpy.linalg.norm(x - xe) / numpy.linalg.norm(xe)
+
+    # Differenza delle 2 memorie calcolate precedentemente (in MB)
+    memoria_usata = memoria_dopo_risoluzione_sistema - memoria_dopo_lettura_matrice
+    
+    # Stampa delle informazioni
+    print("Dimensione:", dimensione)
+    print("Tempo impiegato:", tempo_impiegato)
+    print("Errore relativo:", errore_relativo)
+    print("Memoria usata:", memoria_usata)
+
+# Richiamo della funzione per ogni matrice generica
 # dalla più piccola alla più grande
+#matrix_solver('GT01R.mtx')
+#matrix_solver('TSC_OPF_1047.mtx')
+#matrix_solver('ns3Da.mtx')
+#matrix_solver('ifiss_mat.mtx')
 
-# Matrice generica
-#script_python('GT01R.mtx')
-
-# Matrice generica
-#script_python('TSC_OPF_1047.mtx')
-
-# Matrice generica
-#script_python('ns3Da.mtx')
-
-# Matrice simmetrica e definita positiva
-#script_python('nd24k.mtx')
-
-# Matrice generica
-#script_python('ifiss_mat.mtx')
-
-# Matrice simmetrica e definita positiva
-#script_python('bundle_adj.mtx')
-
-# Matrice simmetrica e definita positiva
-#script_python('Hook_1498.mtx')
-
-# Matrice simmetrica e definita positiva
-#script_python('G3_circuit.mtx')
+# Richiamo della funzione per ogni matrice simmetrica e definita positiva
+# dalla più piccola alla più grande
+#matrix_solver_cholesky('nd24k.mat')
+#matrix_solver_cholesky('bundle_adj.mat')
+#matrix_solver_cholesky('Hook_1498.mat')
+#matrix_solver_cholesky('G3_circuit.mat')
